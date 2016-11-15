1ee3da34f1be42070c7eda44b776b333c70ff55a
Corazon <tdyshin@gmail.com>
2016-11-15 16:03:51 -0500

    Fix template names in views.py and fix project name in wsgi.py

--- game/views.py
+++ game/views.py
@@ -7,15 +7,15 @@ def index(request):
 
 def register(request):
     context = {'text': 'Register here'}
-    return render(request, 'game/index.html', context)
+    return render(request, 'game/register.html', context)
 
 def users(request):
 	context = {'text': 'User list here'}
-	return render(request, 'game/index.html', context)
+	return render(request, 'game/users.html', context)
 
 def user_detail(request, user_id):
 	return HttpResponse("This page will have user details")
 
 def leaderboard(request):
     context = {'text': 'Leaderboard goes here'}
-    return render(request, 'game/index.html', context)
+    return render(request, 'game/leaderboard.html', context)
