f75b10f8ac005f29d2d5bc1bdc15fe3e8d9e91a6
Corazon <tdyshin@gmail.com>
2016-11-15 15:54:40 -0500

    Create navigation bar template and added it to other templates. Update views.py file.

--- game/views.py
+++ game/views.py
@@ -3,17 +3,19 @@ from django.http import HttpResponse
 
 def index(request):
     context = {'text': 'Welcome to our game'}
-    # return HttpResponse("hello")
     return render(request, 'game/index.html', context)
 
 def register(request):
-    return HttpResponse("Register here")
+    context = {'text': 'Register here'}
+    return render(request, 'game/index.html', context)
 
 def users(request):
-    return HttpResponse("Leaderboard goes here")
+	context = {'text': 'User list here'}
+	return render(request, 'game/index.html', context)
 
 def user_detail(request, user_id):
 	return HttpResponse("This page will have user details")
 
 def leaderboard(request):
-    return HttpResponse("Leaderboard goes here")
+    context = {'text': 'Leaderboard goes here'}
+    return render(request, 'game/index.html', context)
