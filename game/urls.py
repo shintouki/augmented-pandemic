328e69891dad609b97f727af26b535166172d02e
Corazon <tdyshin@gmail.com>
2016-11-16 23:57:12 -0500

    Change register template and add simple built in form

--- game/urls.py
+++ game/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = 'game'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^register/$', views.register, name='register'),
     url(r'^users/$', views.users, name='users'),
     url(r'^user_detail/(?P<user_id>\w+)$', views.user_detail, name='user_detail'),
     url(r'^leaderboard/$', views.leaderboard, name='leaderboard'),
