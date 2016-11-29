cc6b59e92d0224100912a5ffb6e9e966af8be0c3
Sharon Huang <shuang003@citymail.cuny.edu>
2016-11-29 12:54:28 -0500

    added login urls

--- game/urls.py
+++ game/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -9,4 +11,8 @@ urlpatterns = [
     url(r'^user_detail/(?P<user_id>\w+)$', views.user_detail, name='user_detail'),
     url(r'^leaderboard/$', views.leaderboard, name='leaderboard'),
     url(r'^play/$', views.play, name='play'),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^admin/', admin.site.urls),
+
 ]
