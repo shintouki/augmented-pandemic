be6dff6105763ff0ffa0e0b43f78916afe748249
Sharon Huang <shuang003@citymail.cuny.edu>
2016-11-30 18:25:16 -0500

    added user profile model and moved image files

--- game/models.py
+++ game/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Location(models.Model):
     location_text = models.CharField(max_length=200)
@@ -6,3 +9,7 @@ class Location(models.Model):
 
     def __str__(self):
         return self.location_text
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    matches_won = models.IntegerField(default=0)
