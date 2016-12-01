a398c69a70414cdb77eade4b74f249cca2fdcae2
Sharon Huang <shuang003@citymail.cuny.edu>
2016-11-30 22:05:21 -0500

    added match fields to location and profile models

--- game/models.py
+++ game/models.py
@@ -5,7 +5,10 @@ from django.dispatch import receiver
 
 class Location(models.Model):
     location_text = models.CharField(max_length=200)
-    infection_rate = models.IntegerField(default=50)
+    # infection_rate = models.IntegerField(default=30)
+    matches_lost = models.IntegerField(default=30)
+    matches_won = models.IntegerField(default=70)
+    total_matches = model.IntegerField(self.matches_won + self.matches_lost)
 
     def __str__(self):
         return self.location_text
@@ -13,3 +16,5 @@ class Location(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     matches_won = models.IntegerField(default=0)
+    matches_lost = models.IntegerField(default=0)
+    total_matches = models.IntegerField(default=0)
