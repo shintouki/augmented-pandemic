0059c857de399eddb7780f46b3e5834741a92485
Corazon <tdyshin@gmail.com>
2016-12-01 11:52:58 -0500

    Add update to database and staticfiles_dirs in settings.py

--- augmented-pandemic/settings.py
+++ augmented-pandemic/settings.py
@@ -14,8 +14,13 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -125,8 +130,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/game'
 
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'].update(db_from_env)
