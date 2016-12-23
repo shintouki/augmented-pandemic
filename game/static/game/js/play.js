c4365fde181919e6dfd4fcf7c55a8d13622c8ea7
Corazon <tdyshin@gmail.com>
2016-12-22 19:13:07 -0500

    Move javascript test directory to main test directory in /game and create some tests that check vars in play.js

--- game/static/game/js/play.js
+++ game/static/game/js/play.js
@@ -19,6 +19,34 @@ $(document).ready(function() {
     });
 });
 
+let ccnyMarkers = [
+      ['Shephard Hall', 40.820536297872856, -73.94823431968689],
+      ['Engineering Building', 40.821591780613375, -73.94790709018707],
+      ['NAC', 40.81967160141943, -73.95081460475922],
+      ['Architecture Building', 40.81763769349211, -73.95049810409546],
+      ['Marshak', 40.819265636754835, -73.9493715763092],
+      ['Sophie Davis Building', 40.82097473120442, -73.9500904083252],
+      ['The Towers(dorm)', 40.81466994350047, -73.95087361335754],
+      ['CUNY Advanced Science Research Center', 40.815666651361695, -73.95009309053421]
+];
+
+let baysideMarkers = [
+    ['Bayside Fields', 40.771913123811196, -73.7852954864502],
+    ['Bayside Highschool', 40.77160436885864, -73.78072500228882],
+    ['OLBS', 40.76622530236848, -73.78520965576172],
+    ['Bay Terrace Shopping Center', 40.77950154452172, -73.77699136734009],
+    ['Auburndale Station', 40.761609683748404, -73.78986597061157]
+];
+
+let safeZoneMarkers = [
+    ['Safe Zone 1', 40.81652125045496, -73.95123839378357],
+    ['Safe Zone 2', 40.82204441518121, -73.94990801811218],
+    ['Safe Zone 3', 40.81866886418722, -73.95164608955383],
+    ['Safe Zone 4', 40.81717487938587, -73.948073387146],
+    ['Safe Zone 5', 40.82123454218155, -73.94617438316345],
+    ['Safe Zone A', 40.76880925874281, -73.79186153411865]
+];
+
 function initMap() {
     let map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 40.82, lng: -73.9493 },
@@ -87,34 +115,6 @@ function initMap() {
     // Browser doesn't support Geolocation
     handleLocationError(false, infoWindow, map.getCenter());
     }
-
-    let ccnyMarkers = [
-      ['Shephard Hall', 40.820536297872856, -73.94823431968689],
-      ['Engineering Building', 40.821591780613375, -73.94790709018707],
-      ['NAC', 40.81967160141943, -73.95081460475922],
-      ['Architecture Building', 40.81763769349211, -73.95049810409546],
-      ['Marshak', 40.819265636754835, -73.9493715763092],
-      ['Sophie Davis Building', 40.82097473120442, -73.9500904083252],
-      ['The Towers(dorm)', 40.81466994350047, -73.95087361335754],
-      ['CUNY Advanced Science Research Center', 40.815666651361695, -73.95009309053421]
-    ];
-
-    let baysideMarkers = [
-        ['Bayside Fields', 40.771913123811196, -73.7852954864502],
-        ['Bayside Highschool', 40.77160436885864, -73.78072500228882],
-        ['OLBS', 40.76622530236848, -73.78520965576172],
-        ['Bay Terrace Shopping Center', 40.77950154452172, -73.77699136734009],
-        ['Auburndale Station', 40.761609683748404, -73.78986597061157]
-    ];
-
-    let safeZoneMarkers = [
-        ['Safe Zone 1', 40.81652125045496, -73.95123839378357],
-        ['Safe Zone 2', 40.82204441518121, -73.94990801811218],
-        ['Safe Zone 3', 40.81866886418722, -73.95164608955383],
-        ['Safe Zone 4', 40.81717487938587, -73.948073387146],
-        ['Safe Zone 5', 40.82123454218155, -73.94617438316345],
-        ['Safe Zone A', 40.76880925874281, -73.79186153411865]
-    ];
 
     let marker;
     let circle;
--- game/test/javascriptTests/playTest.js
+++ game/test/javascriptTests/playTest.js
@@ -9,7 +9,9 @@ describe('Testing play.js', function() {
             });
         });
 
-        describe('checkFunction', function() {
+    });
+
+    describe('handleLocationError function', function() {
             it('should return geolocation failed if handleLocationError is called', function() {
                 var actualResult = handleLocationError();
                 var expectedResult = "Geolocation failed.";
@@ -19,8 +21,22 @@ describe('Testing play.js', function() {
             it('handleLocationError should exist and be function', function() {
                 assert.isFunction(handleLocationError);
             });
+
+    });
+
+
+    describe('checkVars', function() {
+        it('ccnyMarkers should exist and be a variable', function() {
+            assert.isDefined(ccnyMarkers);
         });
-        
+
+        it('baysideMarkers should exist and be a variable', function() {
+            assert.isDefined(baysideMarkers);
+        });
+
+        it('safeZoneMarkers should exist and be a variable', function() {
+            assert.isDefined(safeZoneMarkers);
+        });
     });
 
 });
